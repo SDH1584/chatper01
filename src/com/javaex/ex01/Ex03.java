1430a934576284354a0f3cc4cfe4af50c4a29a03
usesr <usesr@DESKTOP-3RO9PAV>
2021-11-15 11:02:59 +0900

    1/.

---
+++ src/com/javaex/ex01/Ex03.java
@@ -0,0 +1,38 @@
+package com.javaex.ex01;
+
+public class Ex03 {
+
+	public static void main (String[]args) {
+		
+		char ch01='A';//문자 코드값지정
+		char ch02=65;//정수형코드값
+		
+		System.out.println(ch01);
+		System.out.println(ch02);
+
+		boolean b01 =true;
+		boolean b02 =false;
+		
+		
+		System.out.println(b01);
+		System.out.println(b02);
+		
+		int a=3;
+		int b=5;
+		
+		boolean result = a>b;
+		
+		System.out.println(result);
+		
+		char st='안';
+		String str="안녕해";
+		String str2="안녕하다고";
+		
+		System.out.println(st);
+		System.out.println(str);
+		System.out.println(str2);
+
+				
+	}
+
+}
