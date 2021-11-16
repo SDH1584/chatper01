12232c91135a5c0c8c6a0ba59102844157dacdfe
y <y@DESKTOP-HU2OHCI>
2021-11-16 13:13:15 +0900

    ex10

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,21 @@
+package com.javaex.ex01;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+	
+		int myAge;
+		myAge =25;
+		System.out.println(myAge);
+		
+		myAge=26;
+		System.out.println(myAge);
+
+		long bvar = 21478368;
+		
+		int var01 =01;
+		int var02 =20;
+		int var03 =30;
+		
+	}
+}
